dfed8cfb0fd536d89b0c2514141f545da0021a2b
davidnewhall2 <david@sleepers.pro>
2019-07-07 23:04:42 -0700

    generic homebrew formula pusher

--- scripts/formula-deploy.sh
+++ scripts/formula-deploy.sh
@@ -1,27 +1,28 @@
 #!/bin/bash -x
 
-# Deploys a new unpacker-poller.rb formula file to golift/homebrew-tap.
+# Deploys a new homebrew formula file to golift/homebrew-tap.
 # Requires SSH credentials in ssh-agent to work.
 # Run by Travis-CI when a new release is created on GitHub.
+APP=unpacker-poller
 
 if [ -z "$VERSION" ]; then
   VERSION=$TRAVIS_TAG
 fi
 VERSION=$(echo $VERSION|tr -d v)
 
-make unpacker-poller.rb VERSION=$VERSION
+make ${APP}.rb VERSION=$VERSION
 
 if [ -z "$VERSION" ]; then
-  VERSION=$(grep -E 'archive/v.*tar.gz\s*"' unpacker-poller.rb | grep -Eo 'v([0-9]+\.[0-9]+\.[0-9]*)')
+  VERSION=$(grep -E '^\s+url\s+"' ${APP}.rb | cut -d/ -f7 | cut -d. -f1,2,3)
 fi
 
 rm -rf homebrew-mugs
-git config --global user.email "unpacker@auto.releaser"
-git config --global user.name "unpacker-auto-releaser"
+git config --global user.email "${APP}@auto.releaser"
+git config --global user.name "${APP}-auto-releaser"
 git clone git@github.com:golift/homebrew-mugs.git
 
-cp unpacker-poller.rb homebrew-mugs/Formula
+cp ${APP}.rb homebrew-mugs/Formula
 pushd homebrew-mugs
-git commit -m "Update unpacker-poller on Release: ${VERSION}" Formula/unpacker-poller.rb
+git commit -m "Update ${APP} on Release: ${VERSION}" Formula/${APP}.rb
 git push
 popd

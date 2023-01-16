b35d6a379c06214b3925fc74c7f4d4cef9309579
David Newhall II <david@sleepers.pro>
2023-01-15 21:28:32 -0800

    bug fix for linux

--- pkg/unpackerr/tray_other.go
+++ pkg/unpackerr/tray_other.go
@@ -9,6 +9,7 @@ import (
 )
 
 func (u *Unpackerr) startTray() {
+	go u.Run()
 	defer u.Xtractr.Stop() // stop and wait for extractions to finish.
 	signal.Notify(u.sigChan, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
 	u.Printf("[unpackerr] Need help? %s\n=====> Exiting! Caught Signal: %v", helpLink, <-u.sigChan)

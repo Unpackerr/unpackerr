2f9d4654d0299f0f595ab88e679c77b9e980e493
David Newhall II <david@sleepers.pro>
2025-04-21 22:08:48 -0700

    make unpackerr user/group/config overridable on freebsd

--- init/bsd/freebsd.rc.d
+++ init/bsd/freebsd.rc.d
@@ -5,6 +5,13 @@
 # PROVIDE: unpackerr
 # REQUIRE: networking syslog
 # KEYWORD:
+#
+# Add the following line to /etc/rc.conf or use `sysrc` to enable unpackerr.
+# ${unpackerr_enable="YES"}
+# Optionally there are other parameters:
+# ${unpackerr_user="unpackerr"}
+# ${unpackerr_group="unpackerr"}
+# ${unpackerr_config="/usr/local/etc/${real_name}/unpackerr.conf"}
 
 . /etc/rc.subr
 
@@ -12,24 +19,24 @@ name="unpackerr"
 real_name="unpackerr"
 rcvar="unpackerr_enable"
 unpackerr_command="/usr/local/bin/${real_name}"
-unpackerr_user="unpackerr"
-unpackerr_config="/usr/local/etc/${real_name}/unpackerr.conf"
 pidfile="/var/run/${real_name}/pid"
+# Suck in optional exported override variables.
+# ie. add something like the following to this file: export UP_POLLER_DEBUG=true
+unpackerr_env_file="/usr/local/etc/defaults/${real_name}"
 
 # This runs `daemon` as the `unpackerr_user` user.
 command="/usr/sbin/daemon"
 command_args="-P ${pidfile} -r -t ${real_name} -T ${real_name} -l daemon ${unpackerr_command} -c ${unpackerr_config}"
 
 load_rc_config ${name}
-: ${unpackerr_enable:=no}
+: ${unpackerr_enable:=NO}
+: ${unpackerr_user:="unpackerr"}
+: ${unpackerr_group:="unpackerr"}
+: ${unpackerr_config:="/usr/local/etc/${real_name}/unpackerr.conf"}
 
 # Make a place for the pid file.
 mkdir -p $(dirname ${pidfile})
 chown -R $unpackerr_user $(dirname ${pidfile})
-
-# Suck in optional exported override variables.
-# ie. add something like the following to this file: export UP_POLLER_DEBUG=true
-[ -f "/usr/local/etc/defaults/${real_name}" ] && . "/usr/local/etc/defaults/${real_name}"
 
 # Go!
 run_rc_command "$1"

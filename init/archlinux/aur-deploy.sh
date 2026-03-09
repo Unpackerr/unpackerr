63067f2ba9ee4a369fbcc9a2e921bb666a9c0945
David Newhall II <david@sleepers.pro>
2026-03-09 00:53:21 -0700

    Fix url and pkgrel for arch

--- init/archlinux/aur-deploy.sh
+++ init/archlinux/aur-deploy.sh
@@ -36,14 +36,12 @@ sed -e "s/{{VERSION}}/${VERSION}/g" \
     -e "s/{{Iter}}/${ITERATION}/g" \
     -e "s/{{SHA}}/${SHA}/g" \
     -e "s/{{Desc}}/${DESC}/g" \
-    -e "s%{{SOURCE_PATH}}%${SOURCE_PATH}%g" \
     init/archlinux/PKGBUILD.template | tee release_repo/PKGBUILD
 
 sed -e "s/{{VERSION}}/${VERSION}/g" \
     -e "s/{{Iter}}/${ITERATION}/g" \
     -e "s/{{SHA}}/${SHA}/g" \
     -e "s/{{Desc}}/${DESC}/g" \
-    -e "s%{{SOURCE_PATH}}%${SOURCE_PATH}%g" \
     init/archlinux/SRCINFO.template | tee release_repo/.SRCINFO
 
 [ "$1" != "" ] || push_it
